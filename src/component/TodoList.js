d7449d9e7ad99d832a27d502aa4f2ed69c8ab3b6
alish <gozaalisher@gmail.com>
2025-02-05 10:28:27 +0500

    step5 JSX

---
+++ src/component/TodoList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { TodoListItem } from "./TodoListItem";
+export const TodoList = () => {
+    return (
+        <ul>
+            <li><TodoListItem /></li>
+            <li><TodoListItem /></li>
+        </ul>
+    );
+};
---
+++ src/component/TodoListItem.js
@@ -0,0 +1,4 @@
+import React from "react";
+export const TodoListItem = () => {
+    return <span>Drink Cofee</span>
+};
