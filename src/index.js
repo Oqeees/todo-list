40126c210163b304e88add412270141fc221608c
alish <gozaalisher@gmail.com>
2025-02-05 10:39:15 +0500

    step8 Using arrays as components

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-
 import { TodoList } from "./component/TodoList";
 import { SearchPanel } from "./component/SearchPanel";
 import { AppHeader } from "./component/AppHeader";
 
 const App = () => {
 
-    const value = '<script>alert ("")</script>';
-
+    const todoData = [
+        { label: 'Drink Coffee', important: false },
+        { label: 'Implement React Application', important: true },
+        { label: 'Make notes from your stud', important: false },
+        { label: 'Do not drink alchohol', important: false },
+    ];
     return (
         <div>
-            {/*{ isLoggedIn ? null : loginBox }*/}
-            { value }
-            < AppHeader />
+            <AppHeader />
             <SearchPanel />
-            <TodoList/>
+            <TodoList todos =  {todoData} />
         </div>
-    );
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
