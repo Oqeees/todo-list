e21b5eec1893cb2d4027771a59d83cbe398fd623
alish <gozaalisher@gmail.com>
2025-02-05 14:36:16 +0500

    step17 Editing Project directories

--- src/component/ItemStatusFilter/ItemStatusFilter.js
+++ src/component/ItemStatusFilter/ItemStatusFilter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './ItemStatusFilter.css';
+import './ItemStausFilter.css';
 
 const ItemStatusFilter = () => {
     return (
--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-import AppHeader from './component/AppHeader';
-import SearchPanel from './component/SearchPanel';
-import TodoList from './component/TodoList';
-import ItemStatusFilter from './component/ItemStatusFilter';
-
-import './index.css';
-
-const App = () => {
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Build React App', important: true, id: 2 },
-        { label: 'Write notes', important: false, id: 3 },
-        { label: 'Workout', important: false, id: 4 },
-    ];
-
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-            <TodoList todos={todoData} />
-        </div>
-    );
-};
+import App from './component/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
