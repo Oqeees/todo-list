d2cb4c2306f710ac6151342981cdabc0cc066197
alish <gozaalisher@gmail.com>
2025-02-11 22:16:16 +0500

    step24 Refactoring main components with setState() function for events

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -19,7 +19,8 @@ const App = () => {
                 <SearchPanel />
                 <ItemStatusFilter />
             </div>
-            <TodoList todos={todoData} />
+            <TodoList todos={todoData}
+                      onDeleted = { (id) => console.log('del', id)}/>
         </div>
     );
 };
