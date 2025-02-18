cb31cb0fc5dabea9d54bb1a5e767184670fedce0
alish <gozaalisher@gmail.com>
2025-02-18 22:25:30 +0500

    step34 Search component implementation

--- src/component/SearchPanel/SearchPanel.js
+++ src/component/SearchPanel/SearchPanel.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {Component} from "react";
 import './SearchPanel.css';
 
-const SearchPanel = () => {
-    return (
-        <input type="text"
-               className="form-control search-input"
-               placeholder="Type to search" />
-    );
-};
+export default class SearchPanel extends Component {
 
-export default SearchPanel;
+    state = {
+        term: ''
+    };
+
+    onSearchChange = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onSearchChange(term);
+    };
+
+    render() {
+        return (
+            <input type = "text"
+                   className = {"form-control search-input"}
+                   placeholder={"type to search"}
+                   value={ this.state.term}
+                   onChange={this.onSearchChange}/>
+        );
+    }
+}
